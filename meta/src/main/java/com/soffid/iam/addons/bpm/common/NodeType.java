be8ea7dc2c2059f0285d6ed1d86685016a5eaab0
Gabriel Buades <gbuades@soffid.com>
2021-01-22 18:47:05 +0100

    Labels for fork & join

--- meta/src/main/java/com/soffid/iam/addons/bpm/common/NodeType.java
+++ meta/src/main/java/com/soffid/iam/addons/bpm/common/NodeType.java
@@ -4,14 +4,14 @@ import com.soffid.mda.annotation.Enumeration;
 
 @Enumeration
 public class NodeType {
+	public static String NT_START = "start";
 	public static String NT_SCREEN = "screen";
 	public static String NT_GRANT_SCREEN = "grant-screen";
 	public static String NT_MATCH_SCREEN = "match-screen";
 	public static String NT_APPLY = "apply";
 	public static String NT_CUSTOM = "custom";
 	public static String NT_MAIL = "mail";
-	public static String NT_START = "start";
-	public static String NT_END = "end";
 	public static String NT_FORK = "fork";
 	public static String NT_JOIN = "join";
+	public static String NT_END = "end";
 }
