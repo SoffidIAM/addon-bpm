acdc0564c7810dd6edb9a0b2f25c857ffdf29e2f
Gabriel Buades <gbuades@soffid.com>
2021-11-12 07:51:44 +0100

    Fix message translation

--- core/src/main/java/com/soffid/iam/addons/bpm/handler/Messages.java
+++ core/src/main/java/com/soffid/iam/addons/bpm/handler/Messages.java
@@ -6,15 +6,16 @@ import java.util.ResourceBundle;
 public class Messages {
 	private static final String BUNDLE_NAME = "com.soffid.iam.addons.bpm.handler.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
-
 	private Messages() {
 	}
 
 	public static String getString(String key) {
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
+		try
+		{
+			return com.soffid.iam.lang.MessageFactory.getString(BUNDLE_NAME, key);
+		}
+		catch (MissingResourceException e)
+		{
 			return '!' + key + '!';
 		}
 	}
